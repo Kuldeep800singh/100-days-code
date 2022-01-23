a0c3ecc01bebf5f19931a985e787225a1b4f107a
Kuldeep Singh <kul8003995609deep@gmail.com>
2022-01-23 22:02:47 +0530

    Createad a algo folder

---
+++ Algo/searching/clean.bat
@@ -0,0 +1,4 @@
+del .\build\Makefile
+rmdir /s .\build\CMakeFiles\
+del .\build\CMakeCache.txt
+del .\build\cmake_install.cmake
---
+++ Algo/searching/src/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+
+int main(){
+	sf::RenderWindow window(sf::VideoMode(800,600),
+							"DSA testing",
+							sf::Style::Default);
+	sf::Event events;
+	while(window.isOpen()){
+		
+	}
+							
+}
